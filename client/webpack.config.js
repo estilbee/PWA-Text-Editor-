79e3f5d9a21b5f351afb729aadb851e1b15f0197
estilbee <odaouphars@gmail.com>
2023-01-31 12:38:24 -0500

    fixed bug by adjusting relative path in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -26,8 +25,8 @@ module.exports = () => {
       title: 'Webpack Plugin',
     }),
     new InjectManifest({
-      swSrc: './src/sw.js',
-      swDest: './src/sw.js',
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
     }),
     new WebpackPwaManifest({
       fingerprints: false,
